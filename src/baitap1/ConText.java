4849d6531d11ef52ea6099c79d8fb1aa9cb40102
Khiem <vgkhiem.9.8.c2pctrinh@gmail.com>
2020-04-17 19:27:47 +0700

    b1

---
+++ src/baitap1/ConText.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baitap1;
+
+/**
+ *
+ * @author DELL
+ */
+public class ConText {
+    ITinh tinhToan;
+
+    public void setTinhToan(ITinh tinhToan) {
+        this.tinhToan = tinhToan;
+    }
+    
+    public float tinh (float a, float b){
+        return tinhToan.tinh(a, b);
+    }
+}
